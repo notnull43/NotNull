26b940e994b9e7449c7965d24aaf14819d9d2532
adriano.hcfiocco <adriano.hcfiocco@SJR0561292W10-1>
2019-05-07 20:17:12 -0300

    Primeira MODEL

---
+++ CasaNoturna/CadastroMODEL/ClienteMODEL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CadastroMODEL
+{
+    public class ClienteMODEL
+    {
+        #region ATRIBUTOS DOS CLIENTES
+        private int id;
+        private string nome;
+        private int telefone;
+        private string logradouro;
+        private int numero;
+        private string bairro;
+        private string municipio;
+        private string estado;
+        private int dataNascimento;
+        private int cpf;
+        private int rg;
+        private string sexo;
+        #endregion
+
+        #region PROPRIEDADES DOS CLIETES
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public int Telefone { get => telefone; set => telefone = value; }
+        public string Logradouro { get => logradouro; set => logradouro = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string Bairro { get => bairro; set => bairro = value; }
+        public string Municipio { get => municipio; set => municipio = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public int DataNascimento { get => dataNascimento; set => dataNascimento = value; }
+        public int Cpf { get => cpf; set => cpf = value; }
+        public int Rg { get => rg; set => rg = value; }
+        public string Sexo { get => sexo; set => sexo = value; } 
+        #endregion
+
+    } 
+}
---
+++ CasaNoturna/CadastroMODEL/ProdutoMODEL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroMODEL
+{
+    class ProdutoMODEL
+    {
+        #region ATRIBUTOS DOS PRODUTOS
+        private int codigo;
+        private string categoria;
+        private string nome;
+        private string fornecedor;
+        private decimal precoCusto;
+        private decimal precoVenda;
+        private int estoque;
+        #endregion
+
+        #region PROPRIEDADES DOS PRODUTOS
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Fornecedor { get => fornecedor; set => fornecedor = value; }
+        public decimal PrecoCusto { get => precoCusto; set => precoCusto = value; }
+        public decimal PrecoVenda { get => precoVenda; set => precoVenda = value; }
+        public int Estoque { get => estoque; set => estoque = value; } 
+        #endregion
+    }
+
+}
