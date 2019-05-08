a9af45f70a8fdd84d818e609211458b54103f6be
rafael.moreti <rafael.moreti@SJR0561325W10-1>
2019-05-08 20:13:09 -0300

    Commit DAO com Construtor e Conexão com banco

---
+++ CasaNoturna/CadastroDAO/ClienteDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using CadastroMODEL;
+
+namespace CadastroDAO
+{
+    class ClienteDAO
+    {
+        private SqlConnection conexao;
+        private List<ClienteMODEL> repositório;
+
+
+        //Construtor
+        public ClienteDAO(SqlConnection conexao)
+        {
+            this.conexao = conexao;
+            repositório = new List<ClienteMODEL>();
+        }
+
+
+
+    }
+}
