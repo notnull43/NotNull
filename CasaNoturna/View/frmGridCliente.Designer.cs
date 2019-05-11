efb1890da929bfed0f0c0bcd4d8e3e91e43fc51f
Pedro Esquerdo <pedroesquerdo87@gmail.com>
2019-05-10 21:37:09 -0300

    add mais coisa

--- CasaNoturna/View/frmGridCliente.Designer.cs
+++ CasaNoturna/View/frmGridCliente.Designer.cs
@@ -34,9 +34,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1158, 449);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1134, 446);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmGridCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Controle de clientes";
             this.Load += new System.EventHandler(this.frmGridCliente_Load);
             this.ResumeLayout(false);
--- CasaNoturna/View/frmGridCliente.cs
+++ CasaNoturna/View/frmGridCliente.cs
@@ -12,14 +12,71 @@ namespace CadastroFORM
 {
     public partial class frmGridCliente : Form
     {
+        BindingSource binding;
+
         public frmGridCliente()
         {
             InitializeComponent();
+            binding = new BindingSource();
+        }
+
+        private void btnNovoCadastro_Click(object sender, EventArgs e)
+        {
+            frmCadastroCliente cadastro = new frmCadastroCliente("MINHA TELA DE CADASTRO DE CLIENTE");
+            cadastro.ShowDialog();
+            AtualizaGrid();
+        }
+
+        private void AtualizaGrid(object data = null)
+        {
+            binding.DataSource = data ?? BancoDados.Pessoas.Pesquisar();
+            binding.ResetBindings(true);
+            dgvPessoas.DataSource = binding;
         }
 
         private void frmGridCliente_Load(object sender, EventArgs e)
         {
+            this.WindowState = FormWindowState.Maximized;
+        }
 
+        private void frm_pessoas_Load(object sender, EventArgs e)
+        {
+            dgvPessoas.AutoGenerateColumns = false;
+
+            AtualizaGrid();
+        }
+
+        private void btnPesquisarCadastro_Click(object sender, EventArgs e)
+        {
+            AtualizaGrid(BancoDados.Pessoas.Pesquisar(txtPesquisarNome.Text));
+        }
+
+        private void btnPessoasCadastradas_Click(object sender, EventArgs e)
+        {
+            AtualizaGrid();
+        }
+
+        private void dgvPessoas_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //e.RowIndex = Posição da Linha clicada
+            //(CAST)
+            var pessoa = (Pessoa)dgvPessoas.Rows[e.RowIndex].DataBoundItem;
+            frm_cadastroPessoa cadastro = new frm_cadastroPessoa("MINHA TELA DE ALTERAÇÃO DE PESSOA", pessoa.Codigo);
+            cadastro.ShowDialog();
+            AtualizaGrid();
+        }
+
+        private void btnExcluirCadastro_Click(object sender, EventArgs e)
+        {
+            if (dgvPessoas.SelectedRows.Count > 0)
+            {
+                Pessoa pessoa = (Pessoa)dgvPessoas.SelectedRows[0].DataBoundItem;
+                if (MessageBox.Show("Confirma a Exclusão da Pessoa: " + pessoa.Nome, "Exclusão de Pessoas", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    BancoDados.Pessoas.Excluir(pessoa);
+                    AtualizaGrid();
+                }
+            }
         }
     }
 }
